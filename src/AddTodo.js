b0f0d222463a16aedff3094133418bdb2b6b4aab
godseun_mac <alcy1024@gmail.com>
2022-05-21 02:52:05 +0900

    add button apply

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -6,17 +6,42 @@ class AddTodo extends React.Component {
   constructor(props) {
     super(props);
     this.state = { item: { title: "" } };
+    this.add = props.add;
   }
+
+  onButtonClick = () => {
+    if (this.state.item && "" != this.state.item.title.trim()) {
+      this.add(this.state.item);
+      this.setState({ item: { title: "" } });
+    }
+  };
+
+  onInputChange = (e) => {
+    const thisItem = this.state.item;
+    thisItem.title = e.target.value;
+    this.setState({ item: thisItem });
+    console.log(thisItem);
+  };
 
   render() {
     return (
       <Paper style={{ margin: 16, padding: 16 }}>
         <Grid container>
           <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-            <TextField placeholder="Add Todo here" fullWidth />
+            <TextField
+              placeholder="Add Todo here"
+              onChange={this.onInputChange}
+              value={this.state.item.title}
+              fullWidth
+            />
           </Grid>
           <Grid xs={1} md={1} item>
-            <Button fullWidth color="secondary" variant="outlined">
+            <Button
+              color="secondary"
+              variant="outlined"
+              onClick={this.onButtonClick}
+              fullWidth
+            >
               +
             </Button>
           </Grid>
