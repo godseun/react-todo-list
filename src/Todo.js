eaf7d9a43c2abbd4cf9fa6a758c7838e62b0097d
godseun_mac <alcy1024@gmail.com>
2022-05-21 03:01:08 +0900

    delete event apply

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-import { ListItem, ListItemText, InputBase, Checkbox } from "@material-ui/core";
+import {
+  ListItem,
+  ListItemText,
+  InputBase,
+  Checkbox,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core";
+import { DeleteOutlined } from "@material-ui/icons";
 
 class Todo extends React.Component {
   constructor(props) {
@@ -8,7 +16,12 @@ class Todo extends React.Component {
     this.state = {
       item: props.item,
     };
+    this.delete = props.delete;
   }
+
+  deleteEventHandler = () => {
+    this.delete(this.state.item);
+  };
 
   render() {
     const item = this.state.item;
@@ -26,6 +39,15 @@ class Todo extends React.Component {
             fullWidth={true}
           />
         </ListItemText>
+
+        <ListItemSecondaryAction>
+          <IconButton
+            aria-label="Delete Todo"
+            onClick={this.deleteEventHandler}
+          >
+            <DeleteOutlined />
+          </IconButton>
+        </ListItemSecondaryAction>
       </ListItem>
     );
   }
