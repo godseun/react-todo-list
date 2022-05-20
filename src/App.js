5c4e5c1a41515bb683af6b682b7bde079309cfa6
godseun_mac <alcy1024@gmail.com>
2022-05-21 01:08:47 +0900

    use state var

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Todo from './Todo';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {id:0, title: "Hello World 1", done: true},
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todo item={this.state.item} />
       </div>
     )
   }
