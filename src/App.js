45838e24a28e444f791f91a99b048bb141a0a547
godseun_mac <alcy1024@gmail.com>
2022-05-25 22:20:59 +0900

    nav bar && bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { call } from "./service/ApiService";
+import { call, signout } from "./service/ApiService";
 
 import Todo from "./Todo";
 import AddTodo from "./AddTodo";
-import { Paper, List, Container } from "@material-ui/core";
+import {
+  Paper,
+  List,
+  Container,
+  Grid,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import "./App.css";
 
 class App extends React.Component {
@@ -11,12 +20,13 @@ class App extends React.Component {
     super(props);
     this.state = {
       items: [],
+      loading: true,
     };
   }
 
   componentDidMount() {
     call("/todo", "GET", null).then((response) =>
-      this.setState({ items: response.data })
+      this.setState({ items: response.data, loading: false })
     );
   }
 
@@ -26,7 +36,7 @@ class App extends React.Component {
     );
   };
 
-  uodate = (item) => {
+  update = (item) => {
     call("/todo", "PUT", item).then((response) =>
       this.setState({ items: response.data })
     );
@@ -53,14 +63,42 @@ class App extends React.Component {
         </List>
       </Paper>
     );
-    return (
-      <div className="App">
+
+    const navigationBar = (
+      <AppBar position="static">
+        <Toolbar>
+          <Grid justifyContent="space-between" container>
+            <Grid item>
+              <Typography variant="h6">오늘의 할일</Typography>
+            </Grid>
+            <Grid>
+              <Button color="inherit" onClick={signout}>
+                로그아웃
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    );
+
+    const todoListPage = (
+      <div>
+        {navigationBar}
         <Container maxWidth="md">
           <AddTodo add={this.add} />
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
     );
+
+    const loadingPage = <h1> 로딩중.. </h1>;
+
+    let content = loadingPage;
+
+    if (!this.state.loading) {
+      content = todoListPage;
+    }
+    return <div className="App">{content}</div>;
   }
 }
 
